343f2a813e1355b9982f0606976f45f8fa0b6c77
Seth Battis <sbattis@ml-tc-24-sbattis.local>
2024-06-07 09:50:45 -0400

    fix build

--- bin/assets.mjs
+++ bin/assets.mjs
@@ -1,19 +1,20 @@
-import cli from "@battis/qui-cli";
-import path from "path";
+import cli from '@battis/qui-cli';
+import path from 'path';
 
 const args = cli.init({
   args: {
     positionals: true,
     options: {
       target: {
-        short: "t",
-        description: "Target directory for asset files",
-        default: "assets",
-      },
-    },
-  },
+        short: 't',
+        description: 'Target directory for asset files',
+        default: 'assets'
+      }
+    }
+  }
 });
 
+cli.shell.mkdir('-p', path.resolve(args.values.target));
 args.positionals.forEach((fileName) => {
-  cli.shell.cp("-R", path.resolve(fileName), path.resolve(args.values.target));
+  cli.shell.cp('-R', path.resolve(fileName), path.resolve(args.values.target));
 });
